e4134949a3babc77f36fe26873b3aac5d2e7e88f
Ben Comber <bfgcomber@gmail.com>
2023-08-25 15:54:45 +0100

    extension  made public

--- Sources/Toolbox/UIKitHelpers/UIColor/UIColor+hex.swift
+++ Sources/Toolbox/UIKitHelpers/UIColor/UIColor+hex.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-extension UIColor {
+public extension UIColor {
     convenience init(hex: String) {
         var hexString = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
         if hexString.hasPrefix("#") {
