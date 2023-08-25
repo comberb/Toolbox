d98ce280123afb6e857e6b940b80f933b5d1d209
Ben Comber <bfgcomber@gmail.com>
2023-08-25 15:19:18 +0100

    Initial commit

--- Tests/ToolboxTests/ToolboxTests.swift
+++ Tests/ToolboxTests/ToolboxTests.swift
@@ -6,6 +6,6 @@ final class ToolboxTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(Toolbox().text, "Hello, World!")
+//        XCTAssertEqual(Toolbox().text, "Hello, World!")
     }
 }
